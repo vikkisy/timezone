4c65c39985f299f38d4359966b3e15297f3a0e34
Vickie <vsysoup@gmail.com>
2017-11-09 22:26:31 -0800

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  
+  time;
+  timeZone=null;
+  pstDate() {
+    var d = new Date();
+    var localTime = d.getTime();
+    var localOffset = d.getTimezoneOffset() * 60000;
+    var utc = localTime + localOffset;
+    var pst = utc + (3600000 * -8);
+    this.time = new Date(pst);
+    this.timeZone = "PST";
+  }
+  mstDate() {
+    var d = new Date();
+    var localTime = d.getTime();
+    var localOffset = d.getTimezoneOffset() * 60000;
+    var utc = localTime + localOffset;
+    var pst = utc + (3600000 * -7);
+    this.time = new Date(pst);
+    var color:boolean = true;
+    this.timeZone = "MST";
+  }
+  cstDate() {
+    var d = new Date();
+    var localTime = d.getTime();
+    var localOffset = d.getTimezoneOffset() * 60000;
+    var utc = localTime + localOffset;
+    var pst = utc + (3600000 * -6);
+    this.time = new Date(pst);
+    this.timeZone = "CST";
+  }
+  estDate() {
+    var d = new Date();
+    var localTime = d.getTime();
+    var localOffset = d.getTimezoneOffset() * 60000;
+    var utc = localTime + localOffset;
+    var pst = utc + (3600000 * -5);
+    this.time = new Date(pst);
+    this.timeZone = "EST";
+  }
+  clearDate() {
+    this.time = 0;
+    this.timeZone = 0;
+  }
+
+  
+
+
 }
